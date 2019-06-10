74a63293907aa423fa103bdb739d03d542dafb63
Sebasitan Johansson <sebbe.johansson1995@gmail.com>
2019-06-10 22:53:55 +0200

    Loading user
    Fixed loading user on start.

--- gamemode/sv_database.lua
+++ gamemode/sv_database.lua
@@ -9,124 +9,128 @@ DEFAULT_PROPS = {}
 
 hook.Add("PlayerInitialSpawn", "CheckAccountsExist", function(ply)
 
+    ply.ProfileLoadStatus = 0
     ply.DbData = {}
-
-	if ( !file.IsDir("fortwars", "DATA") ) then 
-		file.CreateDir("fortwars")
-		print("Accounts directory successfully created!")
-	else
-		print("Account " .. ply:SteamID() .. " has been successfully loaded!")
-	end
-	
-	local files = {}
-	
-	for k, v in pairs (file.Find("fortwars/*.txt", "DATA")) do
-		table.insert(files, v)
-	end
 	
 	local truncatedid = string.gsub(ply:SteamID(), ":", "_")
     
     local steamid = DB.Escape(ply:SteamID())
+    local PlayerFound = false
     
-    /*DB.Query({sql = "SELECT * FROM players WHERE steamid = '"..steamid.."'", callback = function(mData)
-        for k,v in pairs(mData[1]) do
-            ply.DbData[k] = v
+    DB.Query({sql = "SELECT * FROM players WHERE steamid = '"..steamid.."'", callback = 
+        function(mData)
+            if #mData != 0 then
+                PlayerFound = true
+                for k,v in pairs(mData[1]) do
+                    ply.DbData[k] = v
+                end
+            end
+            ply.ProfileLoadStatus = ply.ProfileLoadStatus + 1
         end
-    end})
-    DB.Query({sql = "SELECT * FROM upgrades WHERE steamid = '"..steamid.."'", callback = function(mData)
-        local upgrades = {mData[1]['speed_limit'], mData[1]['health_limit'], mData[1]['energy_limit'], mData[1]['energy_regen'], mData[1]['fall_damage_resistance']}
-        ply.DbData['upgrades'] = upgrades
-    end})
+    })
     
-    PrintTable(ply.DbData)*/
     
-	 -- If account already exists, load my existing data
-	if ( table.HasValue(files, truncatedid..".txt") ) then
-        print("Account file loaded")
-	
-		local fileread = file.Read("fortwars/"..truncatedid..".txt")
-		local tbl = util.JSONToTable(fileread)
-		
-		if table.Count(tbl.stats) < table.Count(DEFAULT_STATS) then
-			table.insert( tbl.stats, 0 )
+    DB.Query({sql = "SELECT * FROM upgrades WHERE steamid = '"..steamid.."'", callback = 
+        function(mData)
+            if #mData != 0 then
+                local upgrades = {mData[1]['speed_limit'], mData[1]['health_limit'], mData[1]['energy_limit'], mData[1]['energy_regen'], mData[1]['fall_damage_resistance']}
+                ply.DbData['upgrades'] = upgrades
+            end
+            ply.ProfileLoadStatus = ply.ProfileLoadStatus + 1
+        end
+    })
+    
+    timer.Create(ply:SteamID().."_WaitProfile",1,60,function()
+		if ply and ply:IsValid() then
+			if ply.ProfileLoadStatus == 2 then
+                // Compltely loaded
+                print("Completely loaded player")
+                
+				timer.Destroy(ply:SteamID().."_WaitProfile")
+				ply.ProfileLoadStatus = nil
+                
+                if !table.Empty(ply.DbData) && PlayerFound then
+                    print("Player was loaded")
+                
+                    local fileread = file.Read("fortwars/"..truncatedid..".txt")
+                    local tbl = util.JSONToTable(fileread)
+                    
+                    if table.Count(ply.DbData) < table.Count(DEFAULT_STATS) then
+                        table.insert( ply.DbData, 0 )
+                    end
+                    if table.Count(ply.DbData) < table.Count(DEFAULT_UPGRADES) then
+                        table.insert( ply.DbData, 0 )
+                    end
+                    
+                    ply.name = ply.DbData.name or ""
+                    ply.cash = ply.DbData.cash or 0
+                    ply.classes = ply.DbData.classes or {1, }
+                    ply.specials = ply.DbData.specials or {1, }
+                    ply.upgrades = ply.DbData.upgrades or DEFAULT_UPGRADES
+                    ply.props = ply.DbData.props or DEFAULT_PROPS
+                    ply.stats = ply.DbData.stats or DEFAULT_STATS
+                    ply.memberlevel = ply.DbData.memberlevel or 1
+                    
+                    net.Start("sendinfo")
+                        net.WriteString(ply.name)
+                        net.WriteInt(ply.cash, 32)
+                        net.WriteTable(ply.classes)
+                        net.WriteTable(ply.specials)
+                        net.WriteTable(ply.upgrades)
+                        net.WriteTable(ply.props)
+                        net.WriteTable(ply.stats)
+                        net.WriteInt(ply.memberlevel, 32)
+                    net.Send(ply)
+                    
+                    //ply:ChatPrint( "Your FortWars account has been loaded!" )
+                    
+                else
+                    print("No player was found")
+                    
+                    --Create a new account upon joining if one does not exist
+                    local tbl = {}
+                    
+                    tbl.name = ply:Name()
+                    tbl.cash = 12000
+                    tbl.classes = {1, }
+                    tbl.specials = {0, }
+                    tbl.upgrades = DEFAULT_UPGRADES
+                    tbl.props = DEFAULT_PROPS
+                    tbl.stats = DEFAULT_STATS
+                    tbl.memberlevel = 1
+                    
+                    net.Start("sendinfo")
+                        net.WriteString(ply.name)
+                        net.WriteInt(ply.cash, 32)
+                        net.WriteTable(ply.classes)
+                        net.WriteTable(ply.specials)
+                        net.WriteTable(ply.upgrades)
+                        net.WriteTable(ply.props)
+                        net.WriteTable(ply.stats)
+                        net.WriteInt(ply.memberlevel, 32)
+                    net.Send(ply)
+                    
+                    ply.SaveAccount()
+                    
+                    ply:ChatPrint( "A FortWars account has been created for you!" )
+                    
+                end
+			end
 		end
-		if table.Count(tbl.upgrades) < table.Count(DEFAULT_UPGRADES) then
-			table.insert( tbl.upgrades, 0 )
-		end
-		
-		ply.name = tbl.name or ""
-		ply.cash = tbl.cash or 0
-		ply.classes = tbl.classes or {1, }
-		ply.specials = tbl.specials or {1, }
-		ply.upgrades = tbl.upgrades or DEFAULT_UPGRADES
-		ply.props = tbl.props or DEFAULT_PROPS
-		ply.stats = tbl.stats or DEFAULT_STATS
-		ply.memberlevel = tbl.memberlevel or 1
-		
-		net.Start("sendinfo")
-			net.WriteString(ply.name)
-			net.WriteInt(ply.cash, 32)
-			net.WriteTable(ply.classes)
-			net.WriteTable(ply.specials)
-			net.WriteTable(ply.upgrades)
-			net.WriteTable(ply.props)
-			net.WriteTable(ply.stats)
-			net.WriteInt(ply.memberlevel, 32)
-		net.Send(ply)
-		
-		//ply:ChatPrint( "Your FortWars account has been loaded!" )
-		
-	else 
-		--Create a new account upon joining if one does not exist
-		local tbl = {}
-		
-		tbl.name = ""
-		tbl.cash = 12000
-		tbl.classes = {1, }
-		tbl.specials = {0, }
-		tbl.upgrades = DEFAULT_UPGRADES
-		tbl.props = DEFAULT_PROPS
-		tbl.stats = DEFAULT_STATS
-		tbl.memberlevel = 1
-		
-		ply.name = ""
-		ply.cash = 12000
-		ply.classes = {1, }
-		ply.specials = {0, }
-		ply.upgrades = DEFAULT_UPGRADES
-		ply.props = DEFAULT_PROPS
-		ply.stats = DEFAULT_STATS
-		ply.memberlevel = 1
-			
-		net.Start("sendinfo")
-			net.WriteString(ply.name)
-			net.WriteInt(ply.cash, 32)
-			net.WriteTable(ply.classes)
-			net.WriteTable(ply.specials)
-			net.WriteTable(ply.upgrades)
-			net.WriteTable(ply.props)
-			net.WriteTable(ply.stats)
-			net.WriteInt(ply.memberlevel, 32)
-		net.Send(ply)
-		
-		local json = util.TableToJSON(tbl)		
-		file.Write("fortwars/"..truncatedid..".txt", json)
-		
-		//ply:ChatPrint( "A FortWars account has been created for you!" )
-	end
+	end)
 end)
 
 if ( !timer.Exists("accountsaveinterval") ) then
 	timer.Create("accountsaveinterval", 1, 0, function() 
 		for k, v in pairs (player.GetAll()) do
-			v:SaveAccount()
+			v:NewSaveAccount()
 		end
 	end)
 end
-
+/*
 hook.Add("PlayerDisconnected", "DisconnectSaveAccount", function(ply)
 	ply:SaveAccount()
-end)
+end)*/
 
 require("tmysql4")
 
@@ -143,7 +147,7 @@ function DB.Setup()
 	print("[MySQL Msg] Checking / Creating Tables")
     print(DB.Connected)
 
-	DB.Query({sql = [[CREATE TABLE IF NOT EXISTS players
+	/*DB.Query({sql = [[CREATE TABLE IF NOT EXISTS players
 		(
 			steamid VARCHAR(20),
 			name VARCHAR(20),
@@ -163,7 +167,7 @@ function DB.Setup()
             fall_damage_resistance INT,
 			Primary key (steamid)
 		)
-	]]})
+	]]})*/
 end
 
 function DB.Reset()
@@ -180,9 +184,9 @@ function DB.Connect()
         print("[MySQL Msg] Connected")
         DB.Connected = true 
     elseif DB.Connected then
-        print("Alredy connected")
+        print("Alredy connected.")
     else
-        print("Something went wrong.")
+        print("Could not initialize database.")
     end
 end
 
@@ -215,7 +219,7 @@ function DB.Query(query)
     end
 end
 DB.Connect()
-/*hook.Add( "OnReloaded" , "OnReloadedHook" , function()
+hook.Add( "OnReloaded" , "OnReloadedHook" , function()
     DB.Disconnect()
     print("Current Connections:")
     PrintTable(tmysql.GetTable()) 
@@ -225,21 +229,5 @@ DB.Connect()
     end
     DB.Connected = false
     DB.Connect()
-end) */
+end)
 
-
-if ( !timer.Exists("accountsaveintervalnew") ) then
-	timer.Create("accountsaveintervalnew", 5, 0, function() 
-		for k, v in pairs (player.GetAll()) do
-			//v:NewSaveAccount()
-		end
-	end)
-end
-
-if ( !timer.Exists("accountloadintervalnew") ) then
-	timer.Create("accountloadintervalnew", 5, 0, function() 
-		for k, v in pairs (player.GetAll()) do
-			v:NewLoadAccount()
-		end
-	end)
-end
